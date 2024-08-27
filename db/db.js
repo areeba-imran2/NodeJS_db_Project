d360d0f2c300445724e2cc48e99ff31eeddb24d9
areeba-imran2 <areebaimran.edu@gmail.com>
2024-08-27 16:36:45 +0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,12 +1,27 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
+// pool.on('connect', () => {
+//     console.log('Connected to the database');
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// Create a new Pool instance using DATABASE_URL
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL
 });
 
 pool.on('connect', () => {
